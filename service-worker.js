6979b6d18808c873267b010bbb6f5e17f750f9cc
ryanlin02 <126218351+ryanlin02@users.noreply.github.com>
2025-06-15 17:36:36 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
+const CACHE_NAME = 'gas-calc-v1';
+const urlsToCache = [
+  '/gas-card-calc/',
+  '/gas-card-calc/index.html',
+  '/gas-card-calc/manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
